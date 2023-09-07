b5939d3f728e5839c0e6b46ec36b657fd5fa0f93
Pablo Guala <133620908+pguala@users.noreply.github.com>
2023-09-07 14:14:29 -0300

    v2
    Estilos, cards boostrap y funcionalidad buscador

--- js/space.js
+++ js/space.js
@@ -1,10 +1,12 @@
-var url = "https://images-api.nasa.gov/search?q=andromeda"
+//Buscador
+var inputBuscar = document.getElementById("inputBuscar");
 var div = document.getElementById("contenedor");
 var button = document.getElementById("btnBuscar");
-
 button.addEventListener("click", mostrarDatos);
 
+//Fetch y mostrar datos
 function mostrarDatos() {
+    var url = "https://images-api.nasa.gov/search?q=" + inputBuscar.value;
     div.innerHTML = "";
     fetch(url)
         .then((response) => {
@@ -14,9 +16,20 @@ function mostrarDatos() {
             console.log(data);
             var datos = data.collection.items;
             for (var i = 0; i < datos.length; i++) {
+                var img = (datos[i].links[0].href);
                 var title = datos[i].data[0].title;
+                var desc = datos[i].data[0].description;
+                var timestamp = datos[i].data[0].date_created;
+                console.log(img + title + desc + timestamp);
                 div.innerHTML +=
-                    `<b>${title}</b>`;
+                    `<div class="card">
+                    <img class="card-img-top" src="`+ img + `">
+                    <div class="card-body">
+                      <h5 class="card-title">`+ title + `</h5>
+                      <p class="card-text">`+ desc + `</p>
+                    </div>
+                    <p class="card-text"><small class="text-muted">`+ timestamp + `</p>
+                  </div>`;
             }
         });
 }
